4c9a9757a384ce18ad8c874dee8c140d5a0a2217
xmlynek <xmlynek@stuba.sk>
2021-10-05 09:09:11 +0200

    feat: implement trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -292,6 +292,9 @@ fork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  // set the same trace mask as in parent
+  np->trace_mask = p->trace_mask;
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
