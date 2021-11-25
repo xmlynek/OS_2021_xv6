dc4b0d38ba1ebf985b9d210a209a782ed47b79a2
xmlynek <xmlynek@stuba.sk>
2021-11-25 18:41:02 +0100

    feat: implements Symlink

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -321,14 +321,37 @@ sys_open(void)
     end_op();
     return -1;
   }
-
-  if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
+   if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
     if(f)
       fileclose(f);
     iunlockput(ip);
     end_op();
     return -1;
   }
+
+ if((ip->type == T_SYMLINK) && !(omode & O_NOFOLLOW)){
+   uint startInum = ip->inum;
+    for(int i = 0; i < 10 && ip->type == T_SYMLINK; i++){
+      char target[MAXPATH];
+      if(readi(ip, 0, (uint64)target, 0, MAXPATH) == 0){
+        iunlockput(ip);
+	end_op();
+	return -1;
+      }
+      iunlockput(ip);
+      ip = namei(target);
+      if(ip == 0){
+	end_op();
+	return -1;
+      }
+      if(ip->inum  == startInum){
+	end_op();
+	return -1;
+      }
+      ilock(ip);
+    }
+  }
+
 
   if(ip->type == T_DEVICE){
     f->type = FD_DEVICE;
@@ -482,5 +505,30 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+
+  int res = writei(ip, 0, (uint64)target, 0, MAXPATH);
+  if(res != MAXPATH)
+    return -1;
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
